55f876c71960eabe65fe6d41e2a9dd3b8367acad
delgag2018 <delgag2rpi@gmail.com>
2021-09-27 13:42:41 -0400

    Made simple camera change script

--- Assets/Scripts/SimpleCameraFollow.cs
+++ Assets/Scripts/SimpleCameraFollow.cs
@@ -9,7 +9,7 @@ public class SimpleCameraFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CameraReverse();
     }
 
     // Update is called once per frame
@@ -17,4 +17,38 @@ public class SimpleCameraFollow : MonoBehaviour
     {
         transform.position = target.position + offset;
     }
+
+    public void CameraDefualt()
+    {
+        offset = new Vector3(0, 0, -10);
+        transform.Rotate(new Vector3(0, 0, 0));
+    }
+
+    public void CameraUpsideDown()
+    {
+        transform.Rotate(new Vector3(0, 0, 180));
+    }
+
+
+    public void CameraOnSide(bool leftside)
+    {
+        if(leftside)
+        {
+            transform.Rotate(new Vector3(0, 0, -90));
+        }
+
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, 90));
+        }        
+    }
+
+
+    public void CameraReverse()
+    {
+        transform.Rotate(new Vector3(0, 180, 0));
+        offset = new Vector3(0, 0, 10);
+    }
+
+
 }
